f621a69f3c8a472d2b1cbaee3ab01c5f96492334
sangeeta dhiman <sangeetadhiman2003@gmail.com>
2026-09-17 11:13:18 +0530

    Manage hold seat functionality

--- app/jobs/expire_hold_job.rb
+++ app/jobs/expire_hold_job.rb
@@ -9,13 +9,22 @@ class ExpireHoldJob < ApplicationJob
       return unless hold.active?
       return unless hold.expired?
 
+      hold.trip_seats.lock.each do |trip_seat|
+        next unless trip_seat.held?
+        next unless trip_seat.hold_id == hold.id
+
+        trip_seat.update!(
+          status: :available,
+          hold_id: nil,
+          held_until: nil
+        )
+      end
+
       hold.update!(
         status: :expired
       )
     end
 
-    Rails.cache.delete_matched(
-      "trip-search*"
-    )
+    Rails.cache.delete_matched("trip-search*")
   end
 end
